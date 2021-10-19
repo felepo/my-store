f14d5cb3cb02b3d534e8a8944ddbf7444f7c4df9
Fernando Poncio <fecrackle@gmail.com>
2021-10-19 00:09:35 -0600

    agregando endpoints de prueba

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola, desde mi primer server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy un nuevo endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port: ' + port);
 });
