a47dfcca50a6a6e110ca1e5e79bb19f0e81670cc
Fernando Poncio <fecrackle@gmail.com>
2021-10-25 23:42:45 -0600

    add endpoint prefix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
 const othersRouter = require('./others.router');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
-  app.use('/', othersRouter);
+  const router =  express.Router();
+
+  // add /api/v1 prefix
+  app.use('/api/v1', router);
+
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/', othersRouter);
 }
 
 module.exports = routerApi;
